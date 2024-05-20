0da273abb24ff24cfdf8472366e41a913aa1a295
daniel-rook <daniel.nolasco@tryrook.io>
2024-05-20 12:49:04 -0600

    added build test to verification scripts

--- scripts/rook_sdk_apple_health_verification.sh
+++ scripts/rook_sdk_apple_health_verification.sh
@@ -4,4 +4,5 @@ dart format packages/rook_sdk_apple_health -o none --set-exit-if-changed &&
 dart analyze packages/rook_sdk_apple_health --fatal-infos &&
 pana packages/rook_sdk_apple_health 2>&1 | tee reports/rook_sdk_apple_health_pana.txt &&
 flutter test packages/rook_sdk_apple_health --file-reporter github:reports/rook_sdk_apple_health_test.txt &&
+flutter build ios --debug &&
 echo "→" rook_sdk_apple_health - Verification - Finished
--- scripts/rook_sdk_health_connect_verification.sh
+++ scripts/rook_sdk_health_connect_verification.sh
@@ -4,4 +4,5 @@ dart format packages/rook_sdk_health_connect -o none --set-exit-if-changed &&
 dart analyze packages/rook_sdk_health_connect --fatal-infos &&
 pana packages/rook_sdk_health_connect 2>&1 | tee reports/rook_sdk_health_connect_pana.txt &&
 flutter test packages/rook_sdk_health_connect --file-reporter github:reports/rook_sdk_health_connect_test.txt &&
+flutter build apk --debug &&
 echo "→" rook_sdk_health_connect - Verification - Finished
