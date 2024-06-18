8b75460070a7fcb54f82389f3e6c309c4b7d4847
daniel-rook <daniel.nolasco@tryrook.io>
2024-06-18 12:56:12 -0600

    renamed NotAuthorizedException to SDKNotAuthorizedException

--- packages/rook_sdk_core/lib/rook_sdk_core.dart
+++ packages/rook_sdk_core/lib/rook_sdk_core.dart
@@ -4,7 +4,7 @@ export 'src/enum/rook_environment.dart';
 export 'src/exception/http_request_exception.dart';
 export 'src/exception/missing_configuration_exception.dart';
 export 'src/exception/missing_permissions_exception.dart';
-export 'src/exception/not_authorized_exception.dart';
+export 'src/exception/sdk_not_authorized_exception.dart';
 export 'src/exception/sdk_not_initialized_exception.dart';
 export 'src/exception/timeout_exception.dart';
 export 'src/exception/user_not_initialized_exception.dart';
