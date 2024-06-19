601c5080838611112dbcdef333f8e2cefa7dc1ca
daniel-rook <daniel.nolasco@tryrook.io>
2024-06-19 15:49:07 -0600

    removed deprecated classes

--- packages/rook_sdk_apple_health/lib/rook_sdk_apple_health.dart
+++ packages/rook_sdk_apple_health/lib/rook_sdk_apple_health.dart
@@ -7,5 +7,3 @@ export 'src/ah_rook_data_sources.dart';
 export 'src/ah_rook_event_manager.dart';
 export 'src/ah_rook_health_permissions_manager.dart';
 export 'src/ah_rook_summary_manager.dart';
-export 'src/ios_calories_tracker.dart';
-export 'src/ios_steps_tracker.dart';
