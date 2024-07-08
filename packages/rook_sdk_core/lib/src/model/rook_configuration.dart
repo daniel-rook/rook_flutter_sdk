451b633c63a3d2614f509cb7d40cbd0c31529fe7
daniel-rook <daniel.nolasco@tryrook.io>
2024-07-08 14:52:50 -0600

    added enableBackgroundSync parameters were changed to required named parameters

--- packages/rook_sdk_core/lib/src/model/rook_configuration.dart
+++ packages/rook_sdk_core/lib/src/model/rook_configuration.dart
@@ -5,16 +5,19 @@ import 'package:rook_sdk_core/src/enum/rook_environment.dart';
 /// * [clientUUID] Client unique identifier provided by ROOK.
 /// * [secretKey] Client secretKey provided by ROOK.
 /// * [environment] Behaviour configuration.
+/// * [enableBackgroundSync] Automatically enables background sync on Apple Health and Auto Sync on Health Connect
 class RookConfiguration {
   final String clientUUID;
   final String secretKey;
   final RookEnvironment environment;
+  final bool enableBackgroundSync;
 
-  RookConfiguration(
-    this.clientUUID,
-    this.secretKey,
-    this.environment,
-  );
+  RookConfiguration({
+    required this.clientUUID,
+    required this.secretKey,
+    required this.environment,
+    required this.enableBackgroundSync,
+  });
 
   @override
   String toString() {
