fc7cffd690e45495ae6d83b51e854dc990b6ba2a
daniel-rook <daniel.nolasco@tryrook.io>
2024-08-06 13:46:30 -0600

    updated android_background_steps example

--- lib/features/sdk_health_connect/android_background_steps.dart
+++ lib/features/sdk_health_connect/android_background_steps.dart
@@ -1,7 +1,9 @@
+import 'package:app_settings/app_settings.dart';
 import 'package:flutter/material.dart';
 import 'package:focus_detector/focus_detector.dart';
 import 'package:logging/logging.dart';
 import 'package:rook_flutter_sdk/common/widget/scrollable_scaffold.dart';
+import 'package:rook_sdk_core/rook_sdk_core.dart';
 import 'package:rook_sdk_health_connect/rook_sdk_health_connect.dart';
 
 const String androidBackgroundStepsRoute = '/android/android-background-steps';
@@ -16,25 +18,12 @@ class AndroidBackgroundSteps extends StatefulWidget {
 class _AndroidBackgroundStepsState extends State<AndroidBackgroundSteps> {
   final Logger logger = Logger('AndroidBackgroundSteps');
 
-  bool isLoading = false;
-  bool isAvailable = false;
-  bool isActive = false;
-  bool hasPermissions = false;
+  bool isStepsServiceAvailable = false;
+  bool hasAndroidPermissions = false;
+  bool androidPermissionsPreviouslyDenied = false;
+  bool isTrackingSteps = false;
 
   int steps = 0;
-
-  @override
-  void initState() {
-    AndroidStepsManager.syncTodayAndroidStepsCount().then((todaySteps) {
-      setState(
-        () => steps = todaySteps,
-      );
-    }).catchError((error) {
-      logger.info('Error obtaining steps: $error');
-    });
-
-    super.initState();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -46,97 +35,127 @@ class _AndroidBackgroundStepsState extends State<AndroidBackgroundSteps> {
         child: Column(
           children: [
             CheckboxListTile(
-              value: isActive,
+              value: isStepsServiceAvailable,
               onChanged: (value) {},
-              title: Text(
-                isActive
-                    ? "Background Steps is running"
-                    : "Background Steps is stopped",
-              ),
+              title: const Text("Steps service available"),
             ),
             CheckboxListTile(
-              value: hasPermissions,
+              value: hasAndroidPermissions,
               onChanged: (value) {},
-              title: Text(
-                hasPermissions
-                    ? "Permissions are granted"
-                    : "Missing permissions!",
-              ),
+              title: const Text("Android permissions"),
             ),
-            const SizedBox(height: 20),
-            if (isAvailable)
+            CheckboxListTile(
+              value: isTrackingSteps,
+              onChanged: (value) {},
+              title: const Text("Steps tracking active"),
+            ),
+            if (isStepsServiceAvailable &&
+                !androidPermissionsPreviouslyDenied &&
+                !hasAndroidPermissions)
               FilledButton(
-                onPressed: hasPermissions
-                    ? null
-                    : AndroidStepsManager.requestPermissions,
-                child: const Text('Request permissions'),
+                onPressed: requestAndroidPermissions,
+                child: const Text("Request Android permissions"),
               ),
-            if (isAvailable)
-              FilledButton(
-                onPressed: isLoading
-                    ? null
-                    : isActive
-                        ? stopStepsService
-                        : startStepsService,
-                child: Text(
-                  isActive ? 'Stop Background Steps' : 'Start Background Steps',
+            if (isStepsServiceAvailable &&
+                androidPermissionsPreviouslyDenied &&
+                !hasAndroidPermissions)
+              const Card(
+                child: Padding(
+                  padding: EdgeInsets.all(12),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Text(
+                        "You previously denied permissions, now you must manually grant them.",
+                      ),
+                      TextButton(
+                        onPressed: AppSettings.openAppSettings,
+                        child: Text("Open application Settings"),
+                      )
+                    ],
+                  ),
                 ),
               ),
-            const SizedBox(height: 20),
-            Text('Total steps of today: $steps'),
+            if (isStepsServiceAvailable &&
+                hasAndroidPermissions &&
+                !isTrackingSteps)
+              FilledButton(
+                onPressed: () {
+                  AndroidStepsManager.enableBackgroundAndroidSteps().then((_) {
+                    checkStepsServiceStatus();
+                  }).catchError((error) {
+                    logger.severe("Error enabling steps: $error");
+                  });
+                },
+                child: const Text("Enable steps tracking"),
+              ),
+            if (isTrackingSteps)
+              FilledButton(
+                onPressed: () {
+                  AndroidStepsManager.disableBackgroundAndroidSteps().then((_) {
+                    checkStepsServiceStatus();
+                  }).catchError((error) {
+                    logger.severe("Error disabling steps: $error");
+                  });
+                },
+                child: const Text("Disable steps tracking"),
+              ),
+            if (isTrackingSteps)
+              FilledButton(
+                onPressed: () {
+                  AndroidStepsManager.syncTodayAndroidStepsCount()
+                      .then((syncedSteps) {
+                    setState(() => steps = syncedSteps);
+                  }).catchError((error) {
+                    logger.severe('Error syncing steps: $error');
+                  });
+                },
+                child: const Text("Sync today steps"),
+              ),
+            if (isTrackingSteps && steps > 0) Text("Steps synced: $steps")
           ],
         ),
       ),
     );
   }
 
-  void checkStepsServiceStatus() async {
-    final isAvailable = await AndroidStepsManager.isAvailable();
+  Future<void> checkStepsServiceStatus() async {
+    final isStepsServiceAvailable = await AndroidStepsManager.isAvailable();
+    final hasAndroidPermissions =
+        await HCRookPermissionsManager.checkAndroidPermissions();
     final isActive = await AndroidStepsManager.isBackgroundAndroidStepsActive();
-    final hasPermissions = await AndroidStepsManager.hasPermissions();
 
     setState(() {
-      isLoading = false;
-
-      this.isAvailable = isAvailable;
-      this.isActive = isActive;
-      this.hasPermissions = hasPermissions;
+      this.isStepsServiceAvailable = isStepsServiceAvailable;
+      this.hasAndroidPermissions = hasAndroidPermissions;
+      isTrackingSteps = isActive;
     });
   }
 
-  void startStepsService() async {
-    setState(
-      () => isLoading = true,
-    );
-
+  void requestAndroidPermissions() async {
     try {
-      await AndroidStepsManager.enableBackgroundAndroidSteps();
+      final shouldRequestPermissions =
+          await HCRookPermissionsManager.shouldRequestAndroidPermissions();
 
-      checkStepsServiceStatus();
+      if (shouldRequestPermissions) {
+        final requestPermissionsStatus =
+            await HCRookPermissionsManager.requestAndroidPermissions();
+
+        final permissionsAlreadyGranted =
+            requestPermissionsStatus == RequestPermissionsStatus.alreadyGranted;
+
+        if (permissionsAlreadyGranted) {
+          setState(
+            () => hasAndroidPermissions = true,
+          );
+        }
+      } else {
+        setState(
+          () => androidPermissionsPreviouslyDenied = true,
+        );
+      }
     } catch (error) {
-      logger.info('Error starting steps service: $error');
-
-      setState(
-        () => isLoading = false,
-      );
-    }
-  }
-
-  void stopStepsService() async {
-    setState(
-      () => isLoading = true,
-    );
-
-    try {
-      await AndroidStepsManager.disableBackgroundAndroidSteps();
-
-      checkStepsServiceStatus();
-    } catch (error) {
-      logger.info('Error stopping steps service: $error');
-
-      setState(
-        () => isLoading = false,
-      );
+      logger.severe('requestAndroidPermissions - error: $error');
     }
   }
 }
