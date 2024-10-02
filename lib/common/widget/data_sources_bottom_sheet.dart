d0a898decb92675cbac962ab37b9de87597d8768
daniel-rook <daniel.nolasco@tryrook.io>
2024-10-02 15:41:23 -0600

    added revoke data sources example

--- lib/common/widget/data_sources_bottom_sheet.dart
+++ lib/common/widget/data_sources_bottom_sheet.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:logging/logging.dart';
+import 'package:rook_sdk_apple_health/rook_sdk_apple_health.dart';
 import 'package:rook_sdk_core/rook_sdk_core.dart';
 import 'package:url_launcher/url_launcher.dart';
 
@@ -11,7 +13,7 @@ Widget dataSourcesBottomSheet(
     itemCount: dataSources.length,
     itemBuilder: (BuildContext context, int index) {
       return ListTile(
-        leading: Image.network(dataSources[index].image),
+        leading: Image.network(dataSources[index].imageUrl),
         title: Text(dataSources[index].name),
         subtitle: Text(
           dataSources[index].description,
@@ -19,7 +21,12 @@ Widget dataSourcesBottomSheet(
           overflow: TextOverflow.ellipsis,
         ),
         trailing: dataSources[index].authorizationUrl == null
-            ? const Text('Connected')
+            ? revokableDataSources.contains(dataSources[index].name)
+                ? TextButton(
+                    onPressed: () => _revokeDataSource(dataSources[index]),
+                    child: const Text('Disconnect'),
+                  )
+                : const Text('Connected')
             : TextButton(
                 onPressed: () =>
                     launchUrl(Uri.parse(dataSources[index].authorizationUrl!)),
@@ -28,4 +35,41 @@ Widget dataSourcesBottomSheet(
       );
     },
   );
+}
+
+final Set<String> revokableDataSources = {
+  "Garmin",
+  "Oura",
+  "Polar",
+  "Fitbit",
+  "Withings",
+  "Whoop",
+};
+
+DataSourceType? _getDataSourceType(DataSource dataSource) {
+  return switch (dataSource.name) {
+    "Garmin" => DataSourceType.garmin,
+    "Oura" => DataSourceType.oura,
+    "Polar" => DataSourceType.polar,
+    "Fitbit" => DataSourceType.fitbit,
+    "Withings" => DataSourceType.withings,
+    "Whoop" => DataSourceType.whoop,
+    _ => null
+  };
+}
+
+void _revokeDataSource(DataSource dataSource) {
+  final dataSourceType = _getDataSourceType(dataSource);
+
+  if (dataSourceType == null) {
+    return;
+  }
+
+  final logger = Logger('dataSourcesBottomSheet');
+
+  AHRookDataSources.revokeDataSource(dataSourceType).then((_) {
+    logger.info('Garmin data source was successfully revoked');
+  }).catchError((error) {
+    logger.info('Garmin data source revoke failed: $error');
+  });
 }
