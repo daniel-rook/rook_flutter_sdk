9926c1dba36ce361fa8dc8ee5f23acd2d2412681
daniel-rook <daniel.nolasco@tryrook.io>
2024-11-20 12:53:44 -0600

    changed protobuf-java to protobuf-javalite

--- packages/rook_sdk_health_connect/proto/generate.sh
+++ packages/rook_sdk_health_connect/proto/generate.sh
@@ -1,4 +1,4 @@
 # Dart
 protoc --dart_out=../lib/src/data/proto protos.proto
 # Java
-protoc --java_out=../android/src/main/kotlin/ protos.proto
+protoc --java_out=lite:../android/src/main/kotlin/ protos.proto
