abffd98fdae63c7b393723d82ce0b495a478a722
daniel-rook <daniel.nolasco@tryrook.io>
2026-01-29 15:17:50 -0600

    formated

--- packages/rook_sdk_samsung_health/playground/lib/main.dart
+++ packages/rook_sdk_samsung_health/playground/lib/main.dart
@@ -13,12 +13,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Plugin example app'),
-        ),
-        body: Center(
-          child: Text('Running on: ${Platform.operatingSystem}'),
-        ),
+        appBar: AppBar(title: const Text('Plugin example app')),
+        body: Center(child: Text('Running on: ${Platform.operatingSystem}')),
       ),
     );
   }
